72473822e70461c59ef6f0057877ec4ecc127cf9
Brook1711 <33785776+Brook1711@users.noreply.github.com>
2020-11-22 16:42:05 +0800

    proceed

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from pyspark.sql import SparkSession
+import pyspark.sql.functions as f
+from pyspark.sql.types import *
+import pandas as pd 
+import geopandas as gpd
+import contextily as ctx
+#%matplotlib inline
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
+
+def add_basemap(ax, zoom, url='http://tile.stamen.com/terrain/tileZ/tileX/tileY.png'):
+    xmin, xmax, ymin, ymax = ax.axis()
+    basemap, extent = ctx.bounds2img(xmin, ymin, xmax, ymax, zoom=zoom, url=url)
+    ax.imshow(basemap, extent=extent, interpolation='bilinear')
+    # restore original x/y limits
+    ax.axis((xmin, xmax, ymin, ymax))
 
 appName = "Python Example - PySpark Read CSV"
 master = 'local'
@@ -9,11 +27,102 @@ spark = SparkSession.builder \
     .appName(appName) \
     .getOrCreate()
 
-# Convert list to data frame
-df = spark.read.option('header',True) \
-                .option('multiLine', True) \
-                .csv('taxidata')
+trip_schema = StructType([
+    StructField('VendorID', StringType()),
+    StructField('lpep_pickup_datetime', TimestampType()),
+    StructField('lpep_dropoff_datetime', TimestampType()),
+    StructField('store_and_fwd_flag', StringType()),
+    StructField('RatecodeID', IntegerType()),
+    StructField('PULocationID', IntegerType()),
+    StructField('DOLocationID', IntegerType()),
+    StructField('passenger_count', IntegerType()),
+    StructField('trip_distance', DoubleType()),
+    StructField('fare_amount', DoubleType()),
+    StructField('extra', DoubleType()),
 
-df.show()
+    StructField('mta_tax', DoubleType()),
+    StructField('tip_amount', DoubleType()),
+    StructField('tolls_amount', DoubleType()),
+    StructField('ehail_fee', DoubleType()),
+    StructField('improvement_surcharge', DoubleType()),
+    StructField('total_amount', DoubleType()),
+    StructField('payment_type', IntegerType()),
+    StructField('trip_type', IntegerType()),
+    StructField('congestion_surcharge', DoubleType()),
+    ])
 
-print(f'Record count is: {df.count()}')
+trip_data = spark.read \
+    .option("header", True) \
+    .schema(trip_schema) \
+    .csv("./data/green/*")
+trip_data.printSchema()
+trip_data.write.mode("overwrite").parquet("./values/taxi_green")
+# trip_data = spark.read.parquet("./values/taxi_green")
+extended_trips = trip_data \
+    .withColumn("pick_date", f.to_date(trip_data["lpep_pickup_datetime"])) \
+    .withColumn("pick_hour", f.hour(trip_data["lpep_pickup_datetime"]))\
+    .withColumn("drop_date", f.to_date(trip_data["lpep_dropoff_datetime"])) \
+    .withColumn("drop_hour", f.hour(trip_data["lpep_dropoff_datetime"])) 
+extended_trips = extended_trips.filter((trip_data["lpep_pickup_datetime"] > '2020-01-01 00:00:00'))
+
+hourly_taxi_trips = extended_trips \
+    .groupBy("pick_date", "pick_hour").agg(
+        f.count(extended_trips["fare_amount"]).alias("trip_count"),
+        f.sum(extended_trips["passenger_count"]).alias("passenger_count"),
+        f.sum(extended_trips["fare_amount"]).alias("fare_amount"),
+        f.sum(extended_trips["tip_amount"]).alias("tip_amount"),
+        f.sum(extended_trips["total_amount"]).alias("total_amount")
+    )
+hourly_taxi_trips.write.mode("overwrite").parquet("./values/taxi-trips-hourly")
+
+daily_taxi_trips = hourly_taxi_trips.groupBy("pick_date").agg(
+    f.sum(hourly_taxi_trips["trip_count"]).alias("trip_count"),
+    f.sum(hourly_taxi_trips["passenger_count"]).alias("passenger_count"),
+    f.sum(hourly_taxi_trips["fare_amount"]).alias("fare_amount"),
+    f.sum(hourly_taxi_trips["tip_amount"]).alias("tip_amount"),
+    f.sum(hourly_taxi_trips["total_amount"]).alias("total_amount")
+)
+daily_taxi_trips = daily_taxi_trips.sort("pick_date")
+daily_taxi_trips.write.mode("overwrite").parquet("./values/daily_taxi_trips")
+
+daily_taxi_trips_pandas = daily_taxi_trips.toPandas()
+plt.figure(0)
+plt.xlabel("date")
+plt.ylabel("passengaer amount")
+plt.plot(daily_taxi_trips_pandas["pick_date"],daily_taxi_trips_pandas["passenger_count"])
+
+plt.savefig("./plot/distribution_six_month.png")
+
+one_day_hourly_taxi_trips = hourly_taxi_trips.groupBy("pick_hour").agg(
+    f.sum(hourly_taxi_trips["trip_count"]).alias("trip_count"),
+    f.sum(hourly_taxi_trips["passenger_count"]).alias("passenger_count"),
+    f.sum(hourly_taxi_trips["fare_amount"]).alias("fare_amount"),
+    f.sum(hourly_taxi_trips["tip_amount"]).alias("tip_amount"),
+    f.sum(hourly_taxi_trips["total_amount"]).alias("total_amount")
+)
+one_day_hourly_taxi_trips = one_day_hourly_taxi_trips.sort("pick_hour")
+
+one_day_hourly_taxi_trips.write.mode("overwrite").parquet("./values/one_day_hourly_taxi_trips")
+
+one_day_hourly_taxi_trips_pandas = one_day_hourly_taxi_trips.toPandas()
+
+plt.figure(1)
+plt.xlabel("hour")
+plt.ylabel("passengaer amount")
+plt.bar(one_day_hourly_taxi_trips_pandas["pick_hour"],one_day_hourly_taxi_trips_pandas["passenger_count"])
+
+plt.savefig("./plot/distribution_in_one_day.png")
+"""
+taxi_green_pdf = trip_data.toPandas()
+taxi_green_pdf.write.parquet("./values/taxi_green_pdf")
+"""
+taxi_trips_sample = trip_data \
+    .sample(0.001) \
+    .cache()
+"""
+quantile = taxi_trips_sample \
+    .filter((taxi_trips_sample["pickup_longitude"] > -75) & (taxi_trips_sample["pickup_longitude"] < -65)) \
+    .filter((taxi_trips_sample["pickup_latitude"] > 35) & (taxi_trips_sample["pickup_latitude"] < 45)) \
+    .stat.approxQuantile(["pickup_longitude", "pickup_latitude"], [0.025,0.975], 0.01)
+"""
+print(f'Record count is: {trip_data.count()}')
